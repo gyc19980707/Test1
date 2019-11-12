8fce911866aeb97fcebd6ca80a6024c845fa794d
guyuncheng <1343350208@qq.com>
2019-11-12 18:52:19 +0800

    修补bug的分支完成了

---
+++ Test-gitHub/src/cn/zj/test/GitHubTest2.java
@@ -0,0 +1,14 @@
+package cn.zj.test;
+
+public class GitHubTest2 {
+
+	public static void main(String[] args) {
+//	  创建用于模拟写新功能
+		System.out.println("我还在写新功能");
+
+		
+		System.out.println("刚刚修bug进来改的地方");
+
+	}
+
+}
---
+++ Test-gitHub/src/cn/zj/test/GitHubTest3.java
@@ -0,0 +1,11 @@
+package cn.zj.test;
+
+public class GitHubTest3 {
+
+	public static void main(String[] args) {
+//		这里进行修补bug操作，操作完成
+		System.out.println("进行修补bug的操作完成了");
+
+	}
+
+}
